4230e0c49674d7ce0a1ccf6f5d441d48686cdfba
Albert Simon <simon.albert75@gmail.com>
2020-04-18 18:59:12 +0200

    Fixed ocr endpoint

--- app/controller/ControladorOCR.php
+++ app/controller/ControladorOCR.php
@@ -7,20 +7,15 @@ class ControladorOCR extends Controlador {
 	public function serve() {
         $path = '/tmp';
         $target_file = $path . basename($_FILES["img"]['name']);
-        $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["img"]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
-        }
+
         //echo '<pre>';
-        echo (new TesseractOCR($_FILES["img"]["tmp_name"]))->run();
+        $isuploadOk = in_array($imageFileType, array('png', 'jpg', 'jpeg'));
+        if(!$isuploadOk) {
+            echo '{"status": False}';
+            http_response_code(400);
+        } else {
+            echo (new TesseractOCR($_FILES["img"]["tmp_name"]))->run();
+        }
 	}
 }
