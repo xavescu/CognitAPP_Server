6a74af3c41fce82a6c31e6ad572e4ea18394769e
Albert <simon.albert75@gmail.com>
2020-04-21 20:24:21 +0200

    Fixed bool

--- app/controller/ControladorRegistro.php
+++ app/controller/ControladorRegistro.php
@@ -8,24 +8,24 @@ class ControladorRegistro extends Controlador {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(402);
             header('Content-Type: application/json');
-            echo '{ "status" : False, "error": 402 }';
+            echo '{ "status" : false, "error": 402 }';
         } else {
             $checkmail = checkEmailExists($email);
             if ($checkmail) {
                 http_response_code(400);
                 header('Content-Type: application/json');
-                echo '{ "status" : False, "error": 400 }';
+                echo '{ "status" : false, "error": 400 }';
             } else {
                 $checkuser = checkUserExists($_POST['user']);
 
                 if ($checkuser) {
                     http_response_code(401);
                     header('Content-Type: application/json');
-                    echo '{ "status" : False, "error": 401 }';
+                    echo '{ "status" : false, "error": 401 }';
                 } else {
                     $inserted = insertUser($email, $_POST['nombre'], $_POST['user'], $_POST['password'], 0);
                     header('Content-Type: application/json');
-                    echo '{ "status" : True }';
+                    echo '{ "status" : true }';
                 }
             }
         }
