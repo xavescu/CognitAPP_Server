67ef77e931b7576d5dc006132eb03518b0166257
Albert Simon <simon.albert75@gmail.com>
2020-05-01 20:35:15 +0200

    Fixed CambiarTema bug

--- app/controller/ControladorCambiarTema.php
+++ app/controller/ControladorCambiarTema.php
@@ -11,8 +11,8 @@ class ControladorCambiarTema extends Controlador {
                 $errs[0] = !updateTemaNombre($_POST['id'], $_POST['nombre']);
             else
                 $errs[0] = true;
-        if (isset($_POST['texto']))
-            $errs[1] = !updateTemaSubject($_POST['id'], $_POST['texto']);
+        if (isset($_POST['asignatura']))
+            $errs[1] = !updateTemaSubject($_POST['id'], $_POST['asignatura']);
 
         $error = $errs[0] || $errs[1];
 
